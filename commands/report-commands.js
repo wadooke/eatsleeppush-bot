28452c2c10494e87b5870f4784c8241504e4d43e
wadooke <47511563+wadooke@users.noreply.github.com>
2026-01-04 01:35:44 +0700

    Add report command handlers for bot

---
+++ commands/report-commands.js
@@ -0,0 +1,13 @@
+// commands/report-commands.js - Report command handlers
+function handleLaporanSekarang(bot, msg) {
+  // Your existing /laporan_sekarang handler code here
+}
+
+function handleDebugGA4(bot, msg, analyticsDataClient) {
+  // Your existing /debug_ga4 handler code here
+}
+
+module.exports = {
+  handleLaporanSekarang,
+  handleDebugGA4
+};
